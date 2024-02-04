957f0149b95fe904b356ba95e91370e3857d3d66
RomuloDsouza <0000402106@senaimgaluno.com.br>
2024-02-04 15:36:32 -0300

    Acerto2

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var prompt = require('prompt-sync')();
 
 
-function calcularPorcentagem(x,y){
+// function calcularPorcentagem(x,y){
     
 
-   x= parseFloat(prompt("Digite o  primeiro numero para ser calculado "))
-   y= parseFloat(prompt("Digite o numero segundo numero "))
-   let pct =(y/x)*100;
-    return` ${pct.toFixed(2)}% de ${x} é ${y}`;
-}
-   console.log( calcularPorcentagem())
+//    x= parseFloat(prompt("Digite o  primeiro numero para ser calculado "))
+//    y= parseFloat(prompt("Digite o numero segundo numero "))
+//    let pct =(y/x)*100;
+//     return` ${pct.toFixed(2)}% de ${x} é ${y}`;
+// }
+//    console.log( calcularPorcentagem())
 
 
-
+let m2=3000
  let metragem;
  let numeroQuarto;
 function calcularImovel(metragem,numeroQuarto){
@@ -26,63 +26,37 @@ function calcularImovel(metragem,numeroQuarto){
 
     let preco;
 switch (numeroQuarto) {
+
     case 1:
-        preco= metragem*1;
+        preco= metragem* m2;
         console.log(`A casa custa R$ ${preco.toFixed(2)}`);
         break;
 
         case 2 :
-            preco=metragem*1.2;
+            preco=metragem*(m2*1.2);
             console.log(`A casa custa R$ ${preco.toFixed(2)}`);
             break;
 
             case 3 :
-                preco=metragem*1.5;
+                preco=metragem* (m2*1.5);
                 console.log(`A casa custa R$ ${preco.toFixed(2)}`);
                 break;    
     default:
         break;
 }
 }
-calcularImovel(metragem,numeroQuarto)
+ calcularImovel(metragem,numeroQuarto)
 
 
-function calcularImovel(metragem, numeroQuarto) {
-let preco;
-
-switch (numeroQuarto) {
-case 1:
-  preco = metragem * 1;
-  console.log(`A casa custa R$ ${preco.toFixed(2)}`);
-  break;
-
-case 2:
-  preco = metragem * 1.2;
-  console.log(`A casa custa R$ ${preco.toFixed(2)}`);
-  break;
-
-case 3:
-  preco = metragem * 1.5;
-  console.log(`A casa custa R$ ${preco.toFixed(2)}`);
-  break;
-
-default:
-  console.log("Número de quartos inválido.");
-  break;
-}
-}
 
 
-calcularImovel(50000,2); 
+// let n1= parseFloat(prompt("Digite o numero para ser calculado "))
+
+// function numeroAoquadrado (n1){
+// n1=n1*n1;
+// console.log(`O numero que quadrado e ${n1}`)
+
+// }
 
 
-let n1= parseFloat(prompt("Digite o numero para ser calculado "))
-
-function numeroAoquadrado (n1){
-n1=n1*n1;
-console.log(`O numero que quadrado e ${n1}`)
-
-}
-
-
-numeroAoquadrado(n1)
+// numeroAoquadrado(n1)
